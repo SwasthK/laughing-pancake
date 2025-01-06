ae52a04df41716770aefa8e54f39349506892a09
Swasthik K <swasth319@gmail.com>
2025-01-06 19:40:46 +0530

    feat: add events page & minor fix of previous components (#3)
    1. Add /events page to view users the posts that they have created
    2. Minor fix HotEvents.tsx , EvenetCard.tsx & FilterBar.tsx & app/page.tsx

--- components/HotEvents.tsx
+++ components/HotEvents.tsx
@@ -2,7 +2,7 @@ import { Link } from "./Link";
 
 export function HotEvents() {
   return (
-    <div className="relative p-6 px-6 flex text-white  gap-7 flex-wrap items-center w-full rounded-lg bg-gradient-to-r from-slate-900 via-slate-800 to-slate-700 ">
+    <div className="relative p-6 px-6 flex text-white  gap-7 flex-wrap items-center w-full rounded-lg bg-gradient-to-r from-slate-900 via-slate-800 to-slate-400 ">
       <div className="h-32 w-60 rounded-md bg-blue-100"></div>
       <div className="flex flex-col gap-3 justify-between h-32 ">
         <div className="flex flex-col gap-2">
